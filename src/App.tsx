f6ddc4a2ae3c3ca4bcbc5decc41500f7e5d7b740
shengping <shengping@gmail.com>
2020-12-23 19:11:12 +0100

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Videoplayerlist from './component/Videoplayerlist';
+import VideoPlayerPage from './component/VideoPlayerPage';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Redirect from="/" to="/home" />
+        <Switch>
+            <Route path="/home" component={Videoplayerlist} />
+            <Route path="/channel" component={VideoPlayerPage} />
+            <Route path="*" component={() => <div>404</div> } />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
